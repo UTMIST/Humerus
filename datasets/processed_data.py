ece4299336751a6474f28058dc6d51e516405119
alstonlo <40709307+alstonlo@users.noreply.github.com>
2020-10-25 14:48:00 -0400

    formatting

--- datasets/processed_data.py
+++ datasets/processed_data.py
@@ -1,19 +1,22 @@
-import pandas as pd
 import re
+
+import numpy as np
+import pandas as pd
+
 
 def clean_text(text):
     try:
-        #print("1")
-        #print(text.lower())
+        # print("1")
+        # print(text.lower())
         if "edit:" in text.lower():
-            #print("cleaned")
-            #print(text)
-            #print("   ")
+            # print("cleaned")
+            # print(text)
+            # print("   ")
             return np.nan
         if "https:" in text or "http:" in text:
-            #print("cleaned")
-            #print(text)
-            #print("   ")
+            # print("cleaned")
+            # print(text)
+            # print("   ")
             return np.nan
 
         text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)
@@ -23,27 +26,31 @@ def clean_text(text):
     except:
         return text
 
+
 # input: dataframe
 def process_r_oneliners(file):
     for index, row in file.iterrows():
-        #print(clean_text(row["Text"]))
+        # print(clean_text(row["Text"]))
         file.loc[index, "Title"] = clean_text(row["Title"])
         file.loc[index, "Text"] = clean_text(row["Text"])
     file.dropna(subset=['Text'] and ['Title'], inplace=True)
     return file
 
+
 # input: file_names = [file_name1, file_name2, file_name3, ...]
 def merge_files(file_names):
     frames = []
     for filename in file_names:
-        file = pd.read_csv(filename,sep=',')
+        file = pd.read_csv(filename, sep=',')
         file = file.drop('Score', 1)
         frames.append(process_r_oneliners(file))
     result = pd.concat(frames)
     print(result.shape[0])
-    result.to_csv("final.txt", sep=",",index=False)
+    result.to_csv("final.txt", sep=",", index=False)
+
 
 def main():
-    filenames = ['r-copypasta-scraped.csv','r-Oneliners-scraped.csv','r-    Showerthoughts-scraped.csv']
+    filenames = ['r-copypasta-scraped.csv',
+                 'r-Oneliners-scraped.csv',
+                 'r-Showerthoughts-scraped.csv']
     merge_files(filenames)
-
