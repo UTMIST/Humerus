5d2008a0ad70cbccee8bd03d8ee51af0be9e06cb
caixunshiren <caixunshiren@gmail.com>
2020-10-28 21:54:01 -0400

    Update return_log_prob.py

--- humerus-bot/return_log_prob.py
+++ humerus-bot/return_log_prob.py
@@ -7,13 +7,14 @@ def log_prob_sentence(sentence):
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2', return_dict=True)
     inputs = tokenizer(sentence, return_tensors="pt")
+    input_ids = inputs['input_ids'].numpy()
     outputs = model(**inputs, labels=inputs["input_ids"])
     logits = outputs.logits
     m = torch.nn.Softmax(dim=1)
-    output = m(logits)
+    output = m(logits).detach().numpy()
     log_prob = 0
-    for i in range(list(output.size())[1]):
-        log_prob = log_prob + np.log(float(output[0][i][0]))
+    for i in range(output.shape[1]):
+        log_prob = log_prob + np.log(float(output[0][i][input_ids[0][i]]))
 
     return log_prob
 
