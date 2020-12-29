a9c107065cdf0da907ef539b7972e3519b5f1e9a
alstonlo <40709307+alstonlo@users.noreply.github.com>
2020-12-29 14:37:05 -0500

    umap

--- humerusbot/data/reduce_dim.py
+++ humerusbot/data/reduce_dim.py
@@ -4,21 +4,33 @@ A script to reduce the dimensionality of the CAH sentence embeddings.
 
 import h5py
 import matplotlib.pyplot as plt
+import umap
 from sklearn.decomposition import PCA
 
 from humerusbot.data.embed_sentences import PARENT_DIR, load_embeddings
 
 PCA_DIR = PARENT_DIR / 'embeddings-pca.h5'
+UMAP_DIR = PARENT_DIR / 'embeddings-umap.h5'
 
 
 def run_pca(dim):
     embeddings = load_embeddings()
-    pca = PCA(n_components=dim)
-    low_dim_embeddings = pca.fit_transform(embeddings)
+    pca_fit = PCA(n_components=dim)
+    components = pca_fit.fit_transform(embeddings)
 
     with h5py.File(PCA_DIR, 'w') as hf:
-        hf.create_dataset('embeddings-pca', data=embeddings)
-    return low_dim_embeddings
+        hf.create_dataset('embeddings-pca', data=components)
+    return components
+
+
+def run_umap(dim):
+    embeddings = load_embeddings()
+    umap_fit = umap.UMAP(n_components=dim, verbose=True)
+    components = umap_fit.fit_transform(embeddings)
+
+    with h5py.File(UMAP_DIR, 'w') as hf:
+        hf.create_dataset('embeddings-umap', data=components)
+    return components
 
 
 def _plot_2d_embeddings(x):
@@ -28,8 +40,8 @@ def _plot_2d_embeddings(x):
 
 
 def main():
-    x = run_pca(dim=2)
-    _plot_2d_embeddings(x[0:1000])
+    x = run_umap(dim=2)
+    _plot_2d_embeddings(x)
 
 
 if __name__ == '__main__':
